3e4a5d35fa72bf8e3763ed84665547312dede3f9
spiuk <p.urbina5@alumnos.santotomas.cl>
2023-12-10 23:57:07 -0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,31 @@ android {
 }
 
 dependencies {
+    /*implementation ("org.osmdroid:osmdroid-android:6.1.8")
+    implementation ("org.osmdroid:osmdroid-wms:6.1.11")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")*/
     implementation ("org.osmdroid:osmdroid-android:6.1.8")
     implementation ("org.osmdroid:osmdroid-wms:6.1.11")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    // implementation("com.android.support:support-v4:28.0.0")
+    //implementation("com.android.support:localbroadcastmanager:28.0.0")
+    implementation ("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+    implementation ("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
+
 
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-analytics")
